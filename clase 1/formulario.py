4a70e233be99a801286e9bf28bde99f27bdee7dd
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2023-01-19 15:42:25 -0500

    ejercicios primera clase

---
+++ clase 1/formulario.py
@@ -0,0 +1,26 @@
+# nombre = input ("Cual es tu nombre? ")
+# apellido = input ("Cual es tu apellido? ")
+# edad = input ("Edad: ")
+
+# print (f"Su nombre es: {nombre}, Apellido: {apellido}, Edad: {edad}")
+
+# print (type(nombre))
+
+# numero_1 = float(input("Ingrese un numero: "))
+# numero_2 = float(input("Ingrese otro numero: "))
+
+# suma = numero_1 + numero_2
+# print (f"La suma es: {suma}")
+
+nombre = input ("Cual es tu nombre? ")
+apellido = input ("Cual es tu apellido? ")
+edad = int(input ("Edad: "))
+
+anio_nacimiento = 2023 - edad
+
+es_mayor = 18 <= edad
+
+print (f"Su nombre es: {nombre}, Apellido: {apellido}, Edad: {edad}, Su año de nacimiento es: {anio_nacimiento}")
+print (f"Es mayor?: {es_mayor}")
+
+
---
+++ clase 1/primer_clase.py
@@ -0,0 +1,51 @@
+cadena = "hola"
+
+# print (cadena)
+
+entero1 = 5
+entero_2 = -62
+
+flotante1 = 2.36
+flotante2 = -2.38
+
+verdadero = True
+falso = False
+
+
+resta = 2 - 3
+multiplicacion = 2 * 3
+
+# booleano1 = entero1 < entero_2
+
+# booleano2 = entero1 == entero_2
+
+# booleano1 and booleano2
+
+suma = 2 + 3 + 7 
+# print (suma)
+
+suma = 23
+# print (suma)
+
+suma = "una suma"
+# print (suma)
+
+booleano1 = entero1 > entero_2
+# print (booleano1)
+
+nombre = "Seva"
+edad = 36
+
+# print (f"Mi nombre es {nombre} y mi edad es {edad}")
+
+# print (nombre, resta)
+
+# print ("Hola, cual es tu nombre?")
+# nombre = input ()
+# print (f"Gusto en conocerte {nombre}")
+
+nombre = input ("Hola, cual es tu nombre?")
+print (f"Gusto en conocerte {nombre}")
+
+
+
